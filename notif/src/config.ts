e4c6f4246e35ee07286b6fa9d31ccff0a82b72a9
BigCubeCat <bigcubecat@yandex.ru>
2023-07-06 15:54:58 +0700

    feat(handler): add express

--- notif/src/config.ts
+++ notif/src/config.ts
@@ -5,20 +5,23 @@ dotenv.config({ path: path.resolve(__dirname, "../config.env") });
 
 interface ENV {
   UPDATE_DELAY: number | undefined;
-  PORT: number | undefined;
+  EXPRESS_PORT: number | undefined;
+  WS_PORT: number | undefined;
   REDIS_URI: string | undefined;
 }
 
 interface Config {
   UPDATE_DELAY: number;
-  PORT: number;
+  EXPRESS_PORT: number;
+  WS_PORT: number;
   REDIS_URI: string;
 }
 
 const getConfig = (): ENV => {
   return {
     UPDATE_DELAY: process.env.UPDATE_DELAY ? Number(process.env.UPDATE_DELAY) : undefined,
-    PORT: process.env.PORT ? Number(process.env.PORT) : undefined,
+    WS_PORT: process.env.WS_PORT ? Number(process.env.WS_PORT) : undefined,
+    EXPRESS_PORT: process.env.EXPRESS_PORT ? Number(process.env.EXPRESS_PORT) : undefined,
     REDIS_URI: process.env.REDIS_URI
   };
 };
--- notif/src/index.ts
+++ notif/src/index.ts
@@ -2,7 +2,7 @@ import config from "./config";
 import { WebSocketServer } from 'ws';
 import { delNotif, getValue, initRedisClient, setValue } from "./redis_client";
 
-const wss = new WebSocketServer({ port: config.PORT });
+const wss = new WebSocketServer({ port: config.WS_PORT });
 initRedisClient(config.REDIS_URI);
 
 
@@ -36,5 +36,31 @@ wss.on('connection', function connection(ws) {
     req().catch(console.error)
   }, config.UPDATE_DELAY);
   ws.send('something');
+});
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+const allowedOrigins = ['*']; // TODO ТОЛЬКО с контейнера с java бэком
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+// Then pass these options to cors:
+app.use(cors(options));
+
+app.use(express.json());
+
+// POST method route
+app.post('/:name', (req, res) => {
+  console.log(req.params.name)
+  res.sendStatus(200);
+})
+
+app.listen(config.EXPRESS_PORT, () => {
+  console.log('Express server listening on port 5000');
 });
 
