430b4aadf30d1bc3888553756d5d054253bed965
BigCubeCat <bigcubecat@yandex.ru>
2023-07-11 15:56:21 +0700

    feat(mongo): mongo setup

--- user_auth/routes/users.js
+++ user_auth/routes/users.js
@@ -10,7 +10,7 @@ router.post('/', async (req, res) => {
 		const user = new User(req.body)
 		await user.save()
 		const token = await user.generateAuthToken()
-		res.status(201).send({user, token})
+		res.status(201).send({ user, token })
 	} catch (error) {
 		res.status(400).send(error)
 	}
@@ -19,15 +19,16 @@ router.post('/', async (req, res) => {
 router.post('/login', async (req, res) => {
 	//Login a registered user
 	try {
-		const {email, password} = req.body
+		const { email, password } = req.body
 		const user = await User.findByCredentials(email, password)
 		if (!user) {
-			return res.status(401).send({error: 'Login failed! Check authentication credentials'})
+			return res.status(401).send({ error: 'Login failed! Check authentication credentials' })
 		}
 		const token = await user.generateAuthToken()
-		res.send({user, token})
+		res.send({ user, token })
 	} catch (error) {
-		res.status(400).send({error: error.message})
+		console.log("here");
+		res.status(400).send({ error: error.message })
 	}
 
 })
